53d71baaf8a828448643023a5bed337176b0d37b
Petech <petekshamitbeyli@gmail.com>
2023-01-19 17:25:59 +0300

    deleted-locks

--- src/Common/MetaHeader/index.tsx
+++ src/Common/MetaHeader/index.tsx
@@ -1,16 +1,18 @@
 import Head from "next/head";
 
 type Props = {
-  title?: string,
-  description?: string,
+  title?: string;
+  description?: string;
 };
 
-
-const MetaHeader: React.FC<Props> = ({ title="UiFry", description="Make the best financial decisions"  }) => {
+const MetaHeader: React.FC<Props> = ({
+  title = "UiFry",
+  description = "Make the best financial decisions",
+}) => {
   return (
     <Head>
       <title>{title}</title>
-      <link rel="icon" href="../../../public/favicon.png" />
+      <link rel="icon" href="../../../public/img/favicon.png" />
       <meta name="description" content={description} />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
@@ -18,7 +20,11 @@ const MetaHeader: React.FC<Props> = ({ title="UiFry", description="Make the best
 
       <meta name="og:type" property="og:type" content="website" />
       <meta name="og:title" property="og:title" content={title} />
-      <meta name="og:description" property="og:description" content={description} />
+      <meta
+        name="og:description"
+        property="og:description"
+        content={description}
+      />
     </Head>
   );
 };
