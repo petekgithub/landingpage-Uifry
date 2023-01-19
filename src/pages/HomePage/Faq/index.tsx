85c0c8a885bcefe5a7f821d71f77e72642b876bc
Petech <petekshamitbeyli@gmail.com>
2023-01-19 17:05:21 +0300

    faq-update

--- src/pages/HomePage/Faq/index.tsx
+++ src/pages/HomePage/Faq/index.tsx
@@ -1,41 +1,58 @@
 import styles from "./styles.module.scss";
 
-
-const Faq = () =>  {
+const Faq = () => {
   return (
     <section id="Faq" className={styles.container}>
       <div className={styles.content}>
-        <div className={styles.title}>
-          <span>FAQ</span>
-          <h1>Frequently Asked Questions</h1>
+        <div className={styles.partOne}>
+          <h1>FAQ</h1>
+          <h3>Frequently Asked Questions</h3>
         </div>
         <div className={styles.innerContent}>
           <div className={styles.left}>
-            <div className='leftOne'>
+            <div className="leftOne">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
-            <div className='leftTwo'>
+            <div className="leftTwo">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
-            <div className='leftThree'>
+            <div className="leftThree">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
           </div>
-          <div className={styles.right}> 
-            <div className='rightOne'>
+          <div className={styles.right}>
+            <div className="rightOne">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
-            <div className='rightTwo'>
+            <div className="rightTwo">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
-            <div className='rightThree'>
+            <div className="rightThree">
               <h3>The best financial accounting app ever!</h3>
-              <p>“Arcu at dictum sapien, mollis. Vulputate sit id accumsan, ultricies. In ultrices malesuada elit mauris.</p>
+              <p>
+                “Arcu at dictum sapien, mollis. Vulputate sit id accumsan,
+                ultricies. In ultrices malesuada elit mauris.
+              </p>
             </div>
           </div>
         </div>
