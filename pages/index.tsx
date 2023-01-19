f94751345fbd07cbc61938a5bc445499e409303c
Petech <petekshamitbeyli@gmail.com>
2023-01-19 17:39:20 +0300

    index.tsx-update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,12 @@
-import MetaHeader from 'Common/MetaHeader';
-import type { NextPage } from 'next';
-import HomePage from 'pages/HomePage';
-
-import Advantages from 'pages/HomePage/Advantages';
-import Customize from 'pages/HomePage/Customize';
-import Faq from 'pages/HomePage/Faq';
-import Features from 'pages/HomePage/Features';
-import Footer from 'pages/HomePage/Footer';
-import Header from 'pages/HomePage/Header';
-import Intro from 'pages/HomePage/Intro';
-import Testimonial from 'pages/HomePage/Testimonial';
-
+import MetaHeader from "Common/MetaHeader";
+import type { NextPage } from "next";
+import HomePage from "pages/HomePage";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <HomePage /> 
+      <HomePage />
     </>
   );
 };
